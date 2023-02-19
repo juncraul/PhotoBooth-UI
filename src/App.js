af5cf3c1fcd95c79d3a04a2df4b7792118d1ed35
raul-junc-lateral <raul.junc@lateralgroup.com>
2023-02-19 16:18:18 +0200

    Added Navbar and pagination

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './pages';
+import About from './pages/about';
+import AnnualReport from './pages/annual';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/annual' element={<AnnualReport/>} />
+      </Routes>
+    </Router>
   );
 }
 
